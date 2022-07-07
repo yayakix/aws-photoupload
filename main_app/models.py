200d3cd7d5f566d1e42c2954b35487db4bc2c236
Iyana Marquez <yayakix@gmail.com>
2022-07-07 16:52:36 -0400

    photo upload button

--- main_app/models.py
+++ main_app/models.py
@@ -58,5 +58,11 @@ class Feeding(models.Model):
 
 
 
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for cat_id: {self.cat_id} @{self.url}"
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -2,18 +2,14 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from .models import Cat, Toy
+from .models import Cat, Toy, Photo
 from .forms import FeedingForm
 
-# Create your views here.
+import uuid
+import boto3
 
-# def home(request):
-#     '''
-#     this is where we return a response
-#     in most cases we  would render a template
-#     and we'll need some data for that template
-#     '''
-#     return HttpResponse('<h1> Hello World </h1>')
+S3_BASE_URL = 'https://s3-us-east-1.amazonaws.com/'
+BUCKET = 'cat-collector-iyana21-swe'
 
 def home(request):
   return render(request, 'home.html')
@@ -53,6 +49,25 @@ def assoc_toy(request, cat_id, toy_id):
 
 def assoc_toy_delete(request, cat_id, toy_id):
   Cat.objects.get(id=cat_id).toys.remove(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+
+def add_photo(request, cat_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+      s3 = boto3.client('s3')
+      # need a unique "key" for S3 / needs image file extension too
+      key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+      # just in case something goes wrong
+      try:
+        s3.upload_fileobj(photo_file, BUCKET, key)
+        # build the full url string
+        url = f"{S3_BASE_URL}{BUCKET}/{key}"
+        # we can assign to cat_id or cat (if you have a cat object)
+        photo = Photo(url=url, cat_id=cat_id)
+        photo.save()
+      except Exception as error:
+        print('An error occurred uploading file to S3' + error)
   return redirect('detail', cat_id=cat_id)
 
 class CatCreate(CreateView):
